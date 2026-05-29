b95f267e660d269d454be0ff7823309e7bb6ef0d
Antoni Roman <antonipaulo.roman@gmail.com>
2026-05-28 20:24:08 -0500

    Part 1

--- db/seed.js
+++ db/seed.js
@@ -1,3 +1,6 @@
+import { faker } from "@faker-js/faker";
+import { createEmployee } from "./queries/employees.js";
+
 import db from "#db/client";
 
 await db.connect();
@@ -6,5 +9,13 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
-  // TODO
+  for (let i = 0; i < 10; i++) {
+    const employee = {
+      name: faker.person.fullName(),
+      birthday: faker.date.birthdate({ min: 1950, max: 2005, mode: "year" }),
+      salary: faker.number.int({ min: 40000, max: 150000 })
+    };
+
+    await createEmployee(employee);
+  }
 }
